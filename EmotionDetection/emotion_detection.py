5f3c3170f53c1cd275aeaa8a15659a15b40932e2
Tuna <anhdt.dsai02@gmail.com>
2026-02-11 05:28:29 -0500

    task 8

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,8 @@ def parse_output(output):
     return output["emotionPredictions"][0]["emotion"]
 
 def emotion_detector(text_to_analyze):
+    if text_to_analyze == None:
+        return None
     response = requests.post(url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict',
                             headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"},
                             json = { "raw_document": { "text": text_to_analyze } })
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""api module"""
 from flask import Flask, jsonify, request
 from flasgger import Swagger
 from EmotionDetection.emotion_detection import emotion_detector
@@ -5,9 +6,14 @@ from EmotionDetection.emotion_detection import emotion_detector
 app = Flask(__name__)
 swagger = Swagger(app)
 @app.route('/emotionDetector', methods=['POST'])
-def emotionDetector():
-    text = request.get_json()["text"]
-    return emotion_detector(text)
+def detector():
+    """Detect emotion"""
+    text = request.get_json().get("text")
+    response = emotion_detector(text)
+
+    if response is None:
+        return jsonify({'error': 'Invalid text! Please try again!'}), 400
+    return response
 
 
 # curl -X POST http://127.0.0.1:5000/emotionDetector \
@@ -15,4 +21,4 @@ def emotionDetector():
 #   -d '{"text": "I love my life"}'
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
